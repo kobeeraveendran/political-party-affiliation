9b099e0feba7f0d3726fd4fbbab0e5b69603fee1
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-11-17 22:37:56 -0500

    update

--- adaboost.py
+++ adaboost.py
@@ -6,9 +6,13 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 from preprocessing_nb import build_dataset
 
+# clf = AdaBoostClassifier(
+#     DecisionTreeClassifier(max_depth = 1), 
+#     n_estimators = 200
+# )
+
 clf = AdaBoostClassifier(
-    DecisionTreeClassifier(max_depth = 1), 
-    n_estimators = 200
+    n_estimators = 200, learning_rate = 1
 )
 
 X_train, y_train, X_test, y_test, _ = build_dataset()
