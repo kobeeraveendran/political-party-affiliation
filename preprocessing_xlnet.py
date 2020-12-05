33e6183d2ce0c27573805504831bc7dba9183e39
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-12-04 19:37:50 -0500

    add simple-transformers preprocessing steps

--- preprocessing_xlnet.py
+++ preprocessing_xlnet.py
@@ -1,3 +1,41 @@
 import pandas as pd
 import numpy as np
+from sklearn.utils import shuffle
 
+def load_data():
+
+    data = []
+
+    with open("datasets/conservative.txt", 'r') as file:
+        lines = file.readlines()
+
+        for line in lines:
+            data.append([line[:-1], 1])
+
+    with open("datasets/democrats.txt", 'r') as file:
+        lines = file.readlines()
+
+        for line in lines:
+            data.append([line[:-1], 0])
+
+    shuffled = shuffle(data)
+
+    # for example in shuffled[:5]:
+    #     print(example[0])
+    #     print(example[1])
+    #     print()
+
+    cutoff = int(len(shuffled) * 0.8)
+
+    train = shuffled[:cutoff]
+    test = shuffled[cutoff:]
+
+    return train, test
+
+if __name__ == "__main__":
+
+    train, test = load_data()
+
+    print(train[:5])
+    print('\n')
+    print(test[:5])
